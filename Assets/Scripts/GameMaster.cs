0df5a513883ef5aaed5c6831d259c976c74dba07
eladeliav <elad02.eliav@gmail.com>
2017-12-09 01:07:59 +0200

    finishedAllMenus + fixed lvl 5

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -42,7 +42,14 @@ public class GameMaster : MonoBehaviour {
 
     public void loadNextLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if ((SceneManager.GetActiveScene().buildIndex + 1) >= SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 
     public void Toggle()
