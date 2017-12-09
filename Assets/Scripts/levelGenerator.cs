74e3f9017434df04c57c761d543d5ae19815859e
eladeliav <elad02.eliav@gmail.com>
2017-12-09 21:13:05 +0200

    added lvl 8 plus made all sprites 100

--- Assets/Scripts/levelGenerator.cs
+++ Assets/Scripts/levelGenerator.cs
@@ -14,6 +14,12 @@ public class levelGenerator : MonoBehaviour {
     [SerializeField]
     private GameObject invisSpikePrefab;
 
+    [SerializeField]
+    private GameObject spikePrefabSmall;
+
+    [SerializeField]
+    private GameObject invisSpikePrefabSmall;
+
     private void Start()
     {
         GenerateLevel();
@@ -40,6 +46,10 @@ public class levelGenerator : MonoBehaviour {
                 if(colorMapping.prefab.Equals(spikePrefab) || colorMapping.prefab.Equals(invisSpikePrefab))
                 {
                     Vector3 pos = new Vector3(x - 20, y - 8.78f,10);
+                    Instantiate(colorMapping.prefab, pos, Quaternion.identity, transform);
+                }else if (colorMapping.prefab.Equals(spikePrefabSmall) || colorMapping.prefab.Equals(invisSpikePrefabSmall))
+                {
+                    Vector3 pos = new Vector3(x - 20, y - 9.1f, 10);
                     Instantiate(colorMapping.prefab, pos, Quaternion.identity, transform);
                 }
                 else
