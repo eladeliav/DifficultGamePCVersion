0806e44a62ca272db55eeef5a7effa5899763a04
CookieOfdoom <elad02.eliav@gmail.com>
2017-12-09 12:26:24 +0200

    added level editor

---
+++ Assets/Scripts/colorToPrefab.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class colorToPrefab{
+
+    public Color color;
+
+    public GameObject prefab;
+	
+}
---
+++ Assets/Scripts/levelGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelGenerator : MonoBehaviour {
+
+    public Texture2D map;
+
+    public colorToPrefab[] colorMappings;
+
+    private void Start()
+    {
+        GenerateLevel();
+    }
+
+    public void GenerateLevel()
+    {
+        for (int x = 0; x < map.width; x++){
+            for (int y = 0; y < map.height;y++){
+                GenerateTile(x, y);
+            }
+        }   
+    }
+
+    public void GenerateTile(int x,int y){
+        Color pixelColor = map.GetPixel(x, y);
+
+        if (pixelColor.a == 0f){
+            return;
+        }
+
+        foreach(colorToPrefab colorMapping in colorMappings){
+            if(colorMapping.color.Equals(pixelColor)){
+                Vector2 pos = new Vector2(x, y);
+                Instantiate(colorMapping.prefab, pos, Quaternion.identity, transform);
+            }
+        }
+
+
+    }
+
+}
